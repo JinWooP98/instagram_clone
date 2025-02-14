240accdbf64ca5d4e3a2ec8518e73dfc87eaf7ec
JinWooP98 <qwdk0406@naver.com>
2025-02-14 18:30:44 +0900

    #08 [FE/feat] make CarouselManager class and define constructor, init method

--- src/main/resources/static/js/components/create-feed-modal.js
+++ src/main/resources/static/js/components/create-feed-modal.js
@@ -1,4 +1,6 @@
 
+import CarouselManager from "../ui/CarouselManager.js";
+
 // step 모듈내에서 전역관리
 let currentStep = 1;
 
@@ -83,7 +85,16 @@ function setUpFileUploadEvents () {
                 return false;
             }
             return true;
-        })
+        });
+
+        // 이미지 슬라이드 생성
+        const step2Carousel = new CarouselManager($modal.querySelector('.preview-container'));
+
+        step2Carousel.init(validFiles);
+
+        const step3Carousel = new CarouselManager($modal.querySelector('.write-container'));
+
+        step3Carousel.init(validFiles);
 
         // 모달 step 2로 이동
         goToStep(2);
